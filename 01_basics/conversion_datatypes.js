4d3a9c874931800365db02ecf2343357845721d5
RISHU RAJ <RR907194@GMAIL.com>
2023-12-06 19:07:13 +0530

    conversion

---
+++ 01_basics/conversion_datatypes.js
@@ -0,0 +1,48 @@
+let marks = 10 ;
+let mark = "101" ;
+let roll = "10abc";
+
+// console.log(typeof marks)
+// console.log(typeof mark)
+// console.log(typeof roll)
+// let valueInNumber = Number(roll);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn = "Rishu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Rishu" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Duniya"
+
+let str3 = str1 + str2
+// console.log(str3);
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
